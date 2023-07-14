57b776750a9c8a869d3d4ab5213ff693fc769ee8
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-07-14 18:27:56 +0700

    commit

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -30,12 +30,12 @@ function Shop(){
         </IonHeader>
         <IonContent className='ion-padding'>
         <IonSearchbar animated={true} showCancelButton="focus" placeholder='Cari Sesuatu' class="custom"></IonSearchbar>
-        <div className='flex flex-wrap justify-between gap-5 mt-3'>
+        <div className='flex flex-wrap justify-center gap-5 mt-3'>
         {
             data.map((item:any)=>{
                 return(
-                    <Link key={item.id} to={`/product/${item.id}`} className='flex flex-col gap-2  w-[90px] h-[125px]'>
-                        <img className='rounded-xl w-[90px] h-[90px] object-cover' src={item.image1} alt="" />
+                    <Link key={item.id} to={`/product/${item.id}`} className='flex flex-col gap-2  w-[95px] h-[125px]'>
+                        <img className='rounded-xl w-[95px] h-[95px] object-cover' src={item.image1} alt="" />
                         <h1 className='text-[10px] font-semibold'>{item.title}</h1>
                         
                         {/* <h1 className='text-sm'>{item.harga_asli}</h1> */}
